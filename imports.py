c1e77d08e3db814f9741fbcd686267290de93a78
Franziska Hellmuth <franziska.hellmuth@geo.uio.no>
2020-05-18 13:47:50 +0200

    necessary python packages

---
+++ imports.py
@@ -0,0 +1,28 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.4.2
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+import xarray as xr
+import numpy as np
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+import seaborn as sns
+from glob import glob
+from scipy import stats
+import matplotlib as mpl
+import Ngl
+import functions as fct
+
+
+
